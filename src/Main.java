aeea729b9c7762e1c2858730628becc589b4cca1
SALWA507 <salwaamzane30@gmail.com>
2025-11-07 17:45:08 +0100

    first upload

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        
+        ArrayList<String> produits = new ArrayList<>(List.of("Eau", "Soda", "Chips", "Chocolat"));
+        ArrayList<Double> prix = new ArrayList<>(List.of(5.0, 8.0, 12.0, 15.0));
+        ArrayList<Integer> stock = new ArrayList<>(List.of(10, 5, 7, 3));
+
+        boolean continuer = true;
+
+        
+        while (continuer) {
+            System.out.println("\n========== DISTRIBUTEUR AUTOMATIQUE =====");
+            System.out.println("Produits disponibles :");
+
+            
+            for (int i = 0; i < produits.size(); i++) {
+                System.out.println((i + 1) + ". " + produits.get(i)
+                        + " - " + prix.get(i) + "£"
+                        + " (Stock : " + stock.get(i) + ")");
+            }
+
+            System.out.println("0. Quitter");
+
+            
+            System.out.print("\nChoisissez un produit (0 pour quitter) : ");
+            int choix = scanner.nextInt();
+
+        
+            if (choix == 0) {
+                System.out.println("Merci ! À bientôt ");
+                continuer = false;
+            } else if (choix < 0 || choix > produits.size()) {
+                System.out.println(" Choix invalide !");
+            } else {
+                System.out.println("Vous avez sélectionné : " + produits.get(choix - 1));
+                System.out.println("(La gestion du paiement et du stock sera ajoutée plus tard.)");
+            }
         }
+
+        scanner.close();
     }
 }
